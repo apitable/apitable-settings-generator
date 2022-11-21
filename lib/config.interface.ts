1391fcfd724e05c85ee299b788d7769fd1fca39a
彭安平 <penganping@vikadata.com>
2022-11-22 01:12:50 +0800

    feat: each table can be configured with a different api_host

--- lib/config.interface.ts
+++ lib/config.interface.ts
@@ -1,9 +1,9 @@
 import { IGetRecordsReqParams } from "apitable";
 
 export enum Format {
-    Array = "array",
-    Rows = "rows",
-    Columns = "columns",
+  Array = "array",
+  Rows = "rows",
+  Columns = "columns",
 }
 
 export interface ITableConfig {
@@ -12,10 +12,18 @@ export interface ITableConfig {
   format: Format;
   id?: boolean;
   params?: IGetRecordsReqParams;
+  create?: boolean;
+  _api_client_config: _IApiClientConfig;
 }
 
 export interface IConfig {
   dirName: string;
   fileName: string;
+  create?: boolean;
   tables: ITableConfig[];
+}
+
+export interface _IApiClientConfig {
+  _host: string,
+  _token: string
 }
